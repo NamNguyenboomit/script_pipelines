0fd9a5e686ba77ee1b65d2cc347ba6a18cd489b2
Nam Hoai Nguyen <namnh3@lotte.net>
2026-03-17 11:10:18 +0700

    Add more schema in customer and product

--- schema.py
+++ schema.py
@@ -67,3 +67,64 @@ store_schema = StructType ([
     StructField("Cust Type", StringType(), True)
 ])
 
+
+# Define customer schema
+customer_schema = StructType ([
+    StructField("Store", StringType(), True),
+    StructField("store cd", StringType(), True),
+    StructField("Old_Card_No", StringType(), True),
+    StructField("New_Card_No", StringType(), True),
+    StructField("Store2", StringType(), True),
+    StructField("Customer Name", StringType(), True),
+    StructField("Store3", StringType(), True),
+    StructField("Cust type", StringType(), True),
+    StructField("remark", StringType(), True),
+    StructField("Address", StringType(), True),
+    StructField("Address Code", StringType(), True),
+    StructField("Old Address (Ward)", StringType(), True),
+    StructField("Old Address (District)", StringType(), True),
+    StructField("Old Address (City)", StringType(), True),
+    StructField("New Address (Ward)", StringType(), True),
+    StructField("New Address (City)", StringType(), True),
+    StructField("Latitude", DecimalType(9,7), True),
+    StructField("Longitude", DecimalType(9,6), True),
+    StructField("Contact person", StringType(), True),
+    StructField("Phone", StringType(), True),
+    StructField("Main/Sub_Card", StringType(), True),
+    StructField("Email", StringType(), True),
+    StructField("Key/Normal Account", StringType(), True),
+    StructField("Non-Active/Active", StringType(), True),
+    StructField("Oc Customer Number", StringType(), True),
+    StructField("Seasonal Event", StringType(), True),
+    StructField("Consent Agree", StringType(), True),
+    StructField("Old Store", StringType(), True),
+    StructField("Add New When", StringType(), True)
+])
+
+
+# Define for division and product category table
+
+division_schema = StructType ([
+    StructField("Division", StringType(), True),
+    StructField("Division Short", StringType(), True),
+    StructField("1-Cat", StringType(), True)
+])
+
+
+# Define product schemas
+product_schema = StructType ([
+    StructField("1-Cat", StringType(), True),
+    StructField("1-cat nm", StringType(), True),
+    StructField("2-Cat", StringType(), True),
+    StructField("2-CAT NM", StringType(), True),
+    StructField("3-Cat", StringType(), True),
+    StructField("3-Cat NM", StringType(), True),
+    StructField("4-Cat", StringType(), True),
+    StructField("4-Cat NM", StringType(), True),
+    StructField("Division Short", StringType(), True),
+    StructField("Prod cd", StringType(), True),
+    StructField("Prod nm", StringType(), True),
+    StructField("Sale cd", StringType(), True),
+    StructFielđ("Ven nm", StringType(), True),
+    StructField("Vendor cd", StringType(), True)
+])
